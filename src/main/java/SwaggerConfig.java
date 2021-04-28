436dcdf45fe69c931168581dabd76e0275153213
Deepanshu Lakra <deepanshu.l@HCL.COM>
2021-04-28 12:05:05 +0530

    made some changes in pom.xml and SwaggerConfig.java

--- src/main/java/SwaggerConfig.java
+++ src/main/java/SwaggerConfig.java
@@ -1,23 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableOpenApi
 @EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+		return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("me.sachingupta.microservices"))
+                .paths(PathSelectors.any())
+                .build();
     }
-	
+
 }
